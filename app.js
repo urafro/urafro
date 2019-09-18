26235e591c887161445d8b2b4366b616397db2bb
tinashy <tinashydaniel@gmail.com>
2019-09-18 23:31:00 +0200

    Setup index, new and create routes

--- app.js
+++ app.js
@@ -31,9 +31,45 @@ let BlogSchema = new mongoose.Schema({
 
 let Blog = new mongoose.model("Blog", BlogSchema);
 
+//Initial Route - Home ROute
 app.get("/", (req, res) => {
     res.render("landing");
 });
+
+//Index Route - Show all Blogs
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, foundBlogs) => {
+        if (err) {
+            console.log("error finding blogs in database :" + err);
+        } else {
+            //console.log(foundBlogs);
+            res.render("index", {
+                foundBlogs: foundBlogs
+            });
+        }
+    });
+});
+
+//New Route - serve the add-new-blog form
+app.get("/blogs/new", (req, res) => {
+    res.render("new");
+});
+
+//Create Route - add new blog to database
+app.post("/blogs", (req, res) => {
+    let newBlog = req.body.blog;
+
+    Blog.create(newBlog, (err, createdBlog) => {
+        if (err) {
+            console.log("error creating blog :" + err);
+        } else {
+            res.redirect("/blogs");
+            //console.log(createdBlog);
+        }
+    });
+});
+
+//Show
 
 //Catch-all Route
 app.get("*", () => console.log("Sorry, page not found! What are you doing with your life?"));
