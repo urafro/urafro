b539de93183c8a8a3a5a4996173eb709f8a68d37
Tinashe Dan <tinashydaniel@gmail.com>
2020-06-10 22:58:38 +0200

    link css to landing.ejs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const express = require("express"),
 const app = express();
 
 //Expecting files from the '/public' dir
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 //Setting view engine to EJS
 app.set("view engine", "ejs");
 //bodyParser
@@ -31,9 +31,6 @@ mongoose.connect("mongodb://localhost:27017/blogApp", {
 app.get("/", (req, res) => {
     res.render("landing");
 });
-
-//Catch-all Route
-app.get("*", () => console.log("Sorry, page not found! What are you doing with your life?"));
 
 //Listening to routes on the local server
 app.listen(port, () => console.log("APP LISTENING ON PORT " + port));
