ae0dfa00cc1c1dd09f769fc8c142575eb27ad784
Tinashe Dan <tinashydaniel@gmail.com>
2020-06-10 21:47:17 +0200

    remove blogSchema from app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express"),
-    app = express(),
     expressSanitizer = require("express-sanitizer"),
     bodyParser = require("body-parser"),
     methodOverride = require("method-override"),
     mongoose = require("mongoose"),
     port = 8080;
 
+const app = express();
+
 //Expecting files from the '/public' dir
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 //Setting view engine to EJS
 app.set("view engine", "ejs");
 //bodyParser
@@ -24,56 +25,6 @@ mongoose.connect("mongodb://localhost:27017/blogApp", {
     useUnifiedTopology: true,
     useFindAndModify: false
 });
-
-//Defining Schemas
-let BlogSchema = new mongoose.Schema({
-    tag: String,
-    body: {
-        header: {
-            mainHeading: String,
-            mainImage: String
-        },
-        content : {
-            // [introParagraph] div goes directly below the main heading with text-muted
-            introParagraph: String,
-            sectionOne: {
-                paragraphOne: String,
-                paragraphTwo: String
-            },
-            
-            sectionTwo: {
-                smallHeading: String,
-                // [paragraphOne] first sentence will be styled "italically" by applying a class to it with css
-                paragraphOne: String,
-                // [highlightedParagraph] div has border to the left
-                highlightedParagraph: String,
-                paragraphThree: String
-            },
-            sectionThree: {
-                // [smallHeading] here is even smaller than that of section 2 in font-size
-                smallHeading: String,
-                // [paragraphOne] is a numbered div
-                paragraphOne: String,
-                paragraphTwo: String,
-                smallHeading: String,
-                // [paragraphThree] div is a list-style type div
-                paragraphThree: String,
-                blogImage: String,
-                blogImageDescription: String,
-                paragraphFour: String
-            },
-            // [sectionFour] div is the conclusion
-            sectionFour: {
-                smallHeading: String,
-                paragraphOne: String
-            }
-        }
-        
-    }    
-
-});
-
-let Blog = new mongoose.model("Blog", BlogSchema);
 
 //Initial Route - Home ROute
 app.get("/", (req, res) => {
---
+++ models/blog/blog.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+//Defining Blog Schema
+let BlogSchema = new mongoose.Schema({
+  tag: String,
+  body: {
+    header: {
+      mainHeading: String,
+      mainImage: String
+    },
+    content: {
+      // [introParagraph] div goes directly below the main heading with text-muted
+      introParagraph: String,
+      sectionOne: {
+        paragraphOne: String,
+        paragraphTwo: String
+      },
+
+      sectionTwo: {
+        smallHeading: String,
+        // [paragraphOne] first sentence will be styled "italically" by applying a class to it with css
+        paragraphOne: String,
+        // [highlightedParagraph] div has border to the left
+        highlightedParagraph: String,
+        paragraphThree: String
+      },
+      sectionThree: {
+        // [smallHeading] here is even smaller than that of section 2 in font-size
+        smallHeading: String,
+        // [paragraphOne] is a numbered div
+        paragraphOne: String,
+        paragraphTwo: String,
+        smallHeading: String,
+        // [paragraphThree] div is a list-style type div
+        paragraphThree: String,
+        blogImage: String,
+        blogImageDescription: String,
+        paragraphFour: String
+      },
+      // [sectionFour] div is the conclusion
+      sectionFour: {
+        smallHeading: String,
+        paragraphOne: String
+      }
+    }
+
+  }
+
+});
+
+module.exports = mongoose.model("Blog", BlogSchema);
