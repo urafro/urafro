605c2859ae997d0efc3da7737dc46cda00ac3f56
Tinashe Dan <tinashydaniel@gmail.com>
2020-06-10 21:34:52 +0200

    update blogSchema

--- app.js
+++ app.js
@@ -24,14 +24,53 @@ mongoose.connect("mongodb://localhost:27017/blogApp", {
     useUnifiedTopology: true,
     useFindAndModify: false
 });
-//Escaping mongoose deprecation warnings
-mongoose.set('useFindandModify', false);
 
 //Defining Schemas
 let BlogSchema = new mongoose.Schema({
-    title: String,
-    image: String,
-    desc: String
+    tag: String,
+    body: {
+        header: {
+            mainHeading: String,
+            mainImage: String
+        },
+        content : {
+            // [introParagraph] div goes directly below the main heading with text-muted
+            introParagraph: String,
+            sectionOne: {
+                paragraphOne: String,
+                paragraphTwo: String
+            },
+            
+            sectionTwo: {
+                smallHeading: String,
+                // [paragraphOne] first sentence will be styled "italically" by applying a class to it with css
+                paragraphOne: String,
+                // [highlightedParagraph] div has border to the left
+                highlightedParagraph: String,
+                paragraphThree: String
+            },
+            sectionThree: {
+                // [smallHeading] here is even smaller than that of section 2 in font-size
+                smallHeading: String,
+                // [paragraphOne] is a numbered div
+                paragraphOne: String,
+                paragraphTwo: String,
+                smallHeading: String,
+                // [paragraphThree] div is a list-style type div
+                paragraphThree: String,
+                blogImage: String,
+                blogImageDescription: String,
+                paragraphFour: String
+            },
+            // [sectionFour] div is the conclusion
+            sectionFour: {
+                smallHeading: String,
+                paragraphOne: String
+            }
+        }
+        
+    }    
+
 });
 
 let Blog = new mongoose.model("Blog", BlogSchema);
