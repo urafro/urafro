314b563fcd4020807d64e455808f9d600c4def27
Tinashe Dan <tinashydaniel@gmail.com>
2020-07-13 16:29:43 +0200

    remove placeholder port in app.js

--- app.js
+++ app.js
@@ -71,6 +71,6 @@ app.use("/blogs/:id/comments", Comment);
 app.use(index);
 
 //Listening to routes on heroku server
-app.listen(process.env.PORT || 3000, process.env.IP, () => {
+app.listen(process.env.PORT, process.env.IP, () => {
     console.log("SERVER STARTED!!");
 });
