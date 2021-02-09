410369487436100e97a6a3e102fe9d093ef46989
Tinashe Dan <tinashydaniel@gmail.com>
2021-02-09 07:04:20 +0200

    add smooth scrolling with jQeury

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
-  $('h1').click(function() {
-    $(this).css('color', '#ff2525');
-  })
+  /* scroll on button click */
+  $('.js--scroll-to-intro').click(() => {
+    $('html, body').animate({
+      scrollTop: $('.js--section--intro').offset().top
+    }, 1000);
+  });
+
+  $('.js--scroll-to-features').click(() => {
+    $('html, body').animate({
+      scrollTop: $('.js--section--features').offset().top-50
+    }, 1000);
+  });
+
+  $('.js--scroll-to-about').click(() => {
+    $('html, body').animate({
+      scrollTop: $('.js--section--about').offset().top
+    }, 1000);
+  });
+
+  $('.js--scroll-to-contact').click(() => {
+    $('html, body').animate({
+      scrollTop: $('.js--section--contact').offset().top
+    }, 1000);
+  });
+  
 });
