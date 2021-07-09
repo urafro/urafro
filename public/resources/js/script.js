6c0abb1519da20088d9b06eb28916a72702133a6
Nashe Dan <tinashydaniel@gmail.com>
2021-07-10 00:27:45 +0200

    landing page redesign | update scroll-to-button destinations

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -2,26 +2,26 @@ $(document).ready(function() {
   /* scroll on button click */
   $('.js--scroll-to-intro').click(() => {
     $('html, body').animate({
-      scrollTop: $('.js--section--intro').offset().top
-    }, 1000);
+      scrollTop: $('.js--section--intro').offset().top-70
+    }, 2000);
   });
 
   $('.js--scroll-to-features').click(() => {
     $('html, body').animate({
       scrollTop: $('.js--section--features').offset().top-50
-    }, 1000);
+    }, 2000);
   });
 
   $('.js--scroll-to-about').click(() => {
     $('html, body').animate({
       scrollTop: $('.js--section--about').offset().top
-    }, 1000);
+    }, 2000);
   });
 
   $('.js--scroll-to-contact').click(() => {
     $('html, body').animate({
       scrollTop: $('.js--section--contact').offset().top
-    }, 1000);
+    }, 2000);
   });
   
 });
