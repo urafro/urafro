8410a82ba8f579bde9e82562740805ef0a9ff5cc
Nashe Dan <tinashydaniel@gmail.com>
2021-10-26 16:33:57 +0200

    reroute user to projects page when clicking unfinished forms

--- routes/ecommerce.js
+++ routes/ecommerce.js
@@ -9,12 +9,22 @@ router.get("/ecommerce/sme", (req, res) => {
   res.render("sme/landing");
 });
 
+//New Route - store user phone number and reroute them to pricing page
+router.post("/ecommerce/sme", (req, res) => {
+  req.flash("success", "Hiya. We are still developing the bits. Kindly reach out to us using our phone number +263 772 794 678");
+  res.redirect("/projects");
+});
+
 //Index Route - show landing page for retailers
 router.get("/ecommerce/retailer", (req, res) => {
   res.render("retailer/landing");
 });
 
-
+//New Route - store user phone number and reroute them to pricing page
+router.post("/ecommerce/retailer", (req, res) => {
+  req.flash("success", "Hiya. We are still developing the bits. Kindly reach out to us using our phone number +263 772 794 678");
+  res.redirect("/projects");
+});
 
 
 module.exports = router;
