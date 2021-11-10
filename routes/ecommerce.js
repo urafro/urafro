bdb0ef3202d83510a2d1a309e6126b601d972625
Nashe Dan <tinashydaniel@gmail.com>
2021-11-10 10:33:22 +0200

    add ecommerce route condition in app.js route calling

--- routes/ecommerce.js
+++ routes/ecommerce.js
@@ -5,34 +5,34 @@ const middleware = require("../middleware");
 //========================== SME ROUTES ========================
 
 //Index Route - show landing page for sme's
-router.get("/ecommerce/sme", (req, res) => {
+router.get("/sme", (req, res) => {
   res.render("sme/landing");
 });
 
 //New Route - store user phone number and reroute them to pricing page
-router.post("/ecommerce/sme", (req, res) => {
+router.post("/sme", (req, res) => {
   //developing just the design for the pricing page minus the phone number collection logic
   res.render("sme/pricing");
 });
 
 //show ecommerce plan prices
-router.get("/ecommerce/pricing", (req, res) => {
+router.get("/pricing", (req, res) => {
   res.render("sme/pricing");
 });
 
 //Index Route - show landing page for retailers
-router.get("/ecommerce/retailer", (req, res) => {
+router.get("/retailer", (req, res) => {
   res.render("retailer/landing");
 });
 
 //New Route - store user phone number and reroute them to pricing page
-router.post("/ecommerce/retailer", (req, res) => {
+router.post("/retailer", (req, res) => {
   req.flash("success", "Hiya. We are still developing the bits. Kindly reach out to us using our phone number +263 772 794 678");
   res.redirect("/projects");
 });
 
 //Index Route - show landing page for wholesalers
-router.get("/ecommerce/wholesalers", (req, res) => {
+router.get("/wholesalers", (req, res) => {
   res.render("wholesaler/landing");
 });
 
