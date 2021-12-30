28181a0e383bab7866d6d2f8e6805be089e4baca
Nashe Dan <tinashydaniel@gmail.com>
2021-12-30 20:51:09 +0200

    create get route for sitemap

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require("express"),
-    expressSanitizer = require("express-sanitizer"),
-    session = require("cookie-session"),
-    bodyParser = require("body-parser"),
-    methodOverride = require("method-override"),
-    mongoose = require("mongoose"),
-    Blog = require("./routes/blog"),
-    Comment = require("./routes/comment"),
-    passport = require("passport"),
-    index = require("./routes/index"),
-    ecommerce = require("./routes/ecommerce"),
-    LocalStrategy = require("passport-local"),
-    LocalStrategyMongoose = require("passport-local-mongoose"),
-    User = require("./models/user"),
-    flash = require("connect-flash"),
-    nodemailer = require("nodemailer"),
-    //figure out how to use moment js to change the date format in index.ejs
-    moment = require('moment'),
-    seedDB = require("./seeds"),
-    port = 8080;
+  expressSanitizer = require("express-sanitizer"),
+  path = require("path"),
+  session = require("cookie-session"),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
+  mongoose = require("mongoose"),
+  Blog = require("./routes/blog"),
+  Comment = require("./routes/comment"),
+  passport = require("passport"),
+  index = require("./routes/index"),
+  ecommerce = require("./routes/ecommerce"),
+  LocalStrategy = require("passport-local"),
+  LocalStrategyMongoose = require("passport-local-mongoose"),
+  User = require("./models/user"),
+  flash = require("connect-flash"),
+  nodemailer = require("nodemailer"),
+  //figure out how to use moment js to change the date format in index.ejs
+  moment = require("moment"),
+  seedDB = require("./seeds"),
+  port = 8080;
 
 const app = express();
 
@@ -30,32 +31,38 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 //bodyParser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 //methodOverride
 app.use(methodOverride("_method"));
 //Setting up flash messages
 app.use(flash());
 
-mongoose.connect(process.env.DATABASEURL || "mongodb://localhost:27017/urafro", {
+mongoose
+  .connect(process.env.DATABASEURL || "mongodb://localhost:27017/urafro", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
-}).then(() => {
+    useFindAndModify: false,
+  })
+  .then(() => {
     console.log("Connected to DB!");
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log("ERROR", err.message);
-});
-
+  });
 
 //Express-session config
-app.use(session({
+app.use(
+  session({
     secret: "we all need some validation :(",
     saveUninitialized: false,
-    resave: false
-}));
+    resave: false,
+  })
+);
 
 //Passport config
 app.use(passport.initialize());
@@ -67,10 +74,10 @@ passport.deserializeUser(User.deserializeUser());
 
 //Declaring global variables
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use("/blogs", Blog);
@@ -78,7 +85,27 @@ app.use("/blogs/:id/comments", Comment);
 app.use(index);
 app.use("/ecommerce", ecommerce);
 
+/*
+TRYING TO SEND SITEMAP.XML AS A HTTP RESPONSE
+*/
+
+//without middleware
+app.get("/sitemap", (req, res) => {
+  let options = {
+    root: path.join(__dirname),
+  };
+
+  let fileName = "sitemap.xml";
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log("Sent:", fileName);
+    }
+  });
+});
+
 //Listening to routes on heroku server
 app.listen(process.env.PORT || 3000, process.env.IP, () => {
-    console.log("SERVER STARTED!!");
+  console.log("SERVER STARTED!!");
 });
